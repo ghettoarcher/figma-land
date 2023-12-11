c40700d758d4e78b098268ab754ccb8a8441838a
ghettoarcher <adnan30062000@gmail.com>
2023-12-11 18:44:24 +0200

    added div

--- src/styles/script.js
+++
@@ -1,9 +0,0 @@
-var myObj = new Object();
-var propertyName = "make";
-
-propertyName1 = "model";
-myObj[propertyName1] = "Mustang";
-
-myObj[propertyName] = "Ford";
-
-console.log(Object.getOwnPropertyNames(myObj));
